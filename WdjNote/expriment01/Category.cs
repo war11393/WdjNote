da6dfd435328f3503150815d5a4d7b8cd36676df
wangwei <1139346699@qq.com>
2017-12-17 16:24:31 +0800

    实验二初成

--- WdjNote/expriment01/Category.cs
+++ WdjNote/expriment01/Category.cs
@@ -1,8 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace WdjNote_01
 {
@@ -19,17 +16,6 @@ namespace WdjNote_01
             notelist = new List<string>();
             notelist.Add("我的笔记");
             notelist.Add("其他");
-        }
-        //添加新的分类
-        public static void AddCategory(string name)
-        {
-
-        }
-
-        //删除已有分类
-        public static void DeleteCategory(int id)
-        {
-
         }
 
         //显示现有分类
---
+++ WdjNote/expriment02/Category02.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WdjNote_01;
+
+namespace WdjNote.expriment02
+{
+    class Category02 : Category
+    {
+        //添加新的分类
+        public static void AddCategory(string name)
+        {
+
+        }
+
+        //删除已有分类
+        public static void DeleteCategory(int id)
+        {
+
+        }
+    }
+}
