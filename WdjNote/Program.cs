db902ed0b46af5819d3d410ad25ea9d98d3dfe3a
DUYIQUAN <1334610525@qq.com>
2017-12-17 21:36:46 +0800

    dyq

--- WdjNote/Program.cs
+++ WdjNote/Program.cs
@@ -1,20 +1,31 @@
 ﻿using System;
 using WdjNote.expriment02;
 using WdjNote_01.expriment1;
-
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WdjNote.expriment03;
 namespace WdjNote_01
 {
     class Program
     {
+        [STAThread]
         static void Main(string[] args)
         {
-            string s = "!";
-            while(s.Equals("y") || s.Equals("Y"))
-            {
-                ShowExperiment2();
-                Console.WriteLine("是否继续？(y或Y继续，其他键退出)");
-                s = Console.ReadLine();
-            }
+            //string s = "!";
+            //while(s.Equals("y") || s.Equals("Y"))
+            //{
+            //    ShowExperiment2();
+            //    Console.WriteLine("是否继续？(y或Y继续，其他键退出)");
+            //    s = Console.ReadLine();
+            //}
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Form1 f = new Form1();
+            FileOprate02 fileOperate = new FileOprate02();
+            f.of += fileOperate.OpenFileToString;
+            f.sf += fileOperate.SaveFile;
+            Application.Run(f);
         }
 
         //展示实验二菜单
@@ -22,15 +33,16 @@ namespace WdjNote_01
         {
             Show02.ShowMainMenu();
             int i = int.Parse(System.Console.ReadLine());
+            FileOperate fileOperate = new FileOperate();
             switch (i)
             {
                 case 1:
                     MyFile f = Show02.CreateFileMenu();
-                    FileOperate.SaveFile(f);
+                    fileOperate.SaveFile(f);
                     break;
                 case 2:
                     string s = Show02.OpenFileMenu();
-                    FileOperate.OpenFile(s);
+                    fileOperate.OpenFile(s);
                     break;
                 case 3:
                     Console.WriteLine("请输入删除路径：");
@@ -65,15 +77,16 @@ namespace WdjNote_01
         {
             Show01.ShowMainMenu();
             int i = int.Parse(System.Console.ReadLine());
+            FileOperate fileOperate = new FileOperate();
             switch (i)
             {
                 case 1:
                     MyFile f = Show01.CreateFileMenu();
-                    FileOperate.SaveFile(f);
+                    fileOperate.SaveFile(f);
                     break;
                 case 2:
                     string s = Show01.OpenFileMenu();
-                    FileOperate.OpenFile(s);
+                    fileOperate.OpenFile(s);
                     break;
                 default:
                     Console.WriteLine("请输入正确指令！");
---
+++ WdjNote/impl/IFileOperate.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WdjNote_01;
+
+namespace WdjNote.impl
+{
+   public  interface IFileOperate
+    {
+        //打开文件接口,供控制台使用
+       void OpenFile(string path);
+
+       //打开文件接口,供窗体应用使用
+       string OpenFileToString(string path);
+
+        //保存文件接口
+         void SaveFile(MyFile f);
+
+        //修改文件分类
+         void UpdateFileCategory(MyFile f, string newpath);
+
+
+    }
+}
