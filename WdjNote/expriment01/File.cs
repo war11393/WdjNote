07201ca7e176a6441b01756f7344a3ca257d36c0
wangwei <1139346699@qq.com>
2017-12-17 15:17:50 +0800

    目录结构修改

--- WdjNote/expriment01/File.cs
+++ WdjNote/expriment01/File.cs
@@ -1,21 +1,13 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 
 namespace WdjNote_01
 {
-    class File
+    class MyFile
     {
-        int _cid;//分类id
-        string _name;//路径+文件名
+        string _name;//路径(包含分类名)+文件名
         string _content;//文件内容
 
-        public int Cid {
-            get { return _cid; }
-            set { _cid = value; }
-        }
         public string Name {
             get { return _name; }
             set { _name = value; }
@@ -24,29 +16,35 @@ namespace WdjNote_01
             get { return _content; }
             set { _content = value; }
         }
+    }
 
+    class FileOperate
+    {
         //这里调用读取分类的方法将打开的文件中的分类值读出来，没有就返回0--默认分类
-        public void OpenFile(string path)
+        public static void OpenFile(string path)
         {
-
+            StreamReader sr = new StreamReader(path);
+            string line;
+            while ((line = sr.ReadLine()) != null)
+            {
+                Console.WriteLine(line);
+            }
         }
 
         //将文件类f保存
-        public void SaveFile(File f)
+        public static void SaveFile(MyFile f)
         {
-            
+            FileStream fs = new FileStream(f.Name, FileMode.Create);
+            StreamWriter sw = new StreamWriter(fs);
+            sw.Write(f.Content);
+            sw.Flush();
+            sw.Close(); fs.Close();
         }
 
         //修改文件分类
-        public void UpdateFileCategory(File f, int cid)
+        public static void UpdateFileCategory(MyFile f, string newpath)
         {
-
-        }
-
-        //读取文件类别
-        public int ReadCategory(string content)
-        {
-            return 0;
+            File.Move(f.Name, newpath);
         }
     }
 }
